63471a34ca63ae5f96dea8e67ef72c8a9b05fc82
RenzoAyvar <renzo_andre17@hotmail.com>
2024-03-16 15:29:26 -0500

    Segunda version descripcion

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ DATABASE = 'database.db'
 def conectar_bd():
     return sqlite3.connect(DATABASE)
 
-@app.route('/')
+@app.route('/eventosAdministrador')
 def index():
     conn = conectar_bd()
     c = conn.cursor()
@@ -17,17 +17,28 @@ def index():
     conn.close()
     return render_template('index.html', eventos=eventos)
 
+@app.route('/eventos')
+def mostrar_eventos():
+    conn = conectar_bd()
+    c = conn.cursor()
+    c.execute('SELECT * FROM eventos_academicos ORDER BY fecha DESC')
+    eventos = c.fetchall()
+    conn.close()
+    return render_template('eventos.html', eventos=eventos)
+
+
 @app.route('/agregar', methods=['POST'])
 def agregar_evento():
     nombre = request.form['nombre']
     fecha = request.form['fecha']
     hora = request.form['hora']
+    descripcion = request.form['descripcion']  
     conn = conectar_bd()
     c = conn.cursor()
-    c.execute('INSERT INTO eventos_academicos (nombre, fecha, hora) VALUES (?, ?, ?)', (nombre, fecha, hora))
+    c.execute('INSERT INTO eventos_academicos (nombre, fecha, hora, descripcion) VALUES (?, ?, ?, ?)', (nombre, fecha, hora, descripcion)) 
     conn.commit()
     conn.close()
-    return redirect('/')
+    return redirect('/eventosAdministrador')
 
 @app.route('/editar/<int:id>', methods=['GET', 'POST'])
 def editar_evento(id):
@@ -42,12 +53,13 @@ def editar_evento(id):
         nombre = request.form['nombre']
         fecha = request.form['fecha']
         hora = request.form['hora']
+        descripcion = request.form['descripcion']  
         conn = conectar_bd()
         c = conn.cursor()
-        c.execute('UPDATE eventos_academicos SET nombre = ?, fecha = ?, hora = ? WHERE id = ?', (nombre, fecha, hora, id))
+        c.execute('UPDATE eventos_academicos SET nombre = ?, fecha = ?, hora = ?, descripcion = ? WHERE id = ?', (nombre, fecha, hora, descripcion, id))  
         conn.commit()
         conn.close()
-        return redirect('/')
+        return redirect('/eventosAdministrador')
 
 @app.route('/nuevo')
 def nuevo_evento():
